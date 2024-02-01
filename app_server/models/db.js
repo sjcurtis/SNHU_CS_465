187dcdc515b557b910ff91ae357d437cd60818f4
sjcurtis <shawncurtis1987@live.com>
2024-01-31 20:19:48 -0500

    Completed module 4

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,48 +1,73 @@
-const mongoose = require('mongoose');
-const host = process.env.DB_HOST || '127.0.0.1'
-const dbURI = 'mongodb://{$host}/travlr';
-const readLine = require('readline');
+const mongoose = require("mongoose");
+const host = process.env.DB_HOST || "127.0.0.1";
+const dbURI = `mongodb://${host}/travlr`;
+const readLine = require("readline");
 
-// avoid 'current Server Discovery and Monitoring engine is deprecated'
-mongoose.set('useUnifiedTopology', true);
+// avoid current server discovery and monitoring engine is depreciated
+mongoose.set("useUnifiedTopology", true);
 
 const connect = () => {
-    setTimeout(() => mongoose.connect(dbURI, {
+  setTimeout(
+    () =>
+      mongoose.connect(dbURI, {
         useNewUrlParser: true,
-        useCreateIndex: true
-    }), 1000);
-}
+        useCreateIndex: true,
+      }),
+    1000
+  );
+};
 
-mongoose.connection.on('connected', () => {
-
+mongoose.connection.on("connected", () => {
+  console.log(`Mongoose connected to ${dbURI}`);
 });
 
-mongoose.connection.on('errorr', err => {
-
+mongoose.connection.on("error", (err) => {
+  console.log(`Mongoose connection error: `, err);
 });
 
-mongoose.connection.on('disconnected', () => {
-
+mongoose.connection.on("disconnected", () => {
+  console.log(`Mongoose disconnected`);
 });
 
-if (process.platform === 'win32') {
-
+if (process.platform == "win32") {
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.on("SIGINT", () => {
+    process.emit("SIGINT");
+  });
 }
 
 const gracefulShutdown = (msg, callback) => {
-
+  mongoose.connection.close(() => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
 };
 
-// For nodemon restarts
-process.once('SIGUSR2', () => {
-
+// nodemon restarts
+process.once("SIGUSR2", () => {
+  gracefulShutdown("nodemon restart", () => {
+    process.kill(process.pid, "SIGUSR2");
+  });
 });
-// For app termination
-process.on('SIGTERM', () => {
 
+// app termination
+process.on("SIGINT", () => {
+  gracefulShutdown("app termination", () => {
+    process.exit(0);
+  });
+});
+
+// Herokue app termination
+process.on("SIGTERM", () => {
+  gracefulShutdown("Heroku app shutdown", () => {
+    process.exit(0);
+  });
 });
 
 connect();
 
-// bring in the Mongoose schema
-require('./travlr');
+// bring in schema
+require("./travlr");
